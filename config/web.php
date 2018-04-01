b086545167b2acb0fc2dc3f9aa54b3fe2c30f74c
Alexey Kulagin <proctoleha@gmail.com>
2018-04-01 09:10:53 +0300

    Add rules in web.php

--- config/web.php
+++ config/web.php
@@ -73,6 +73,18 @@ $config = [
             'class' => 'app\modules\feedback\Feedback',
         ],
     ],
+    'on beforeRequest' => function () {
+        $pathInfo = Yii::$app->request->pathInfo;
+        $query = Yii::$app->request->queryString;
+        if (!empty($pathInfo) && substr($pathInfo, -1) === '/') {
+            $url = '/' . substr($pathInfo, 0, -1);
+            if ($query) {
+                $url .= '?' . $query;
+            }
+            Yii::$app->response->redirect($url, 301);
+            Yii::$app->end();
+        }
+    },
 ];
 
 if (YII_ENV_DEV) {
